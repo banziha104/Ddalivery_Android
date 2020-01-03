b66dac2380b305765b71a339f95412806e29e961
iyeongjun <banziha104@gmail.com>
2020-01-03 19:32:35 +0900

    테스트 세팅

---
+++ app/src/androidTest/java/com/iyeongjoon/nicname/ddalivery/view/actvities/SignUpActivityTest.kt
@@ -0,0 +1,15 @@
+package com.iyeongjoon.nicname.ddalivery.view.actvities
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SignUpActivityTest  {
+
+    @Test
+    fun abc(){
+
+    }
+}
